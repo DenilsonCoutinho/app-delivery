a607daf3dd5cf3a0d4ab84fb59204a37d6e48a53
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2025-07-14 18:46:45 -0300

    ajustando numero de contato

--- src/lib/estabilishmentIsOpen.ts
+++ src/lib/estabilishmentIsOpen.ts
@@ -1,4 +1,4 @@
-export function isWithinWorkingHours(startHour = 5, endHour = 18): boolean {
+export function isWithinWorkingHours(startHour = 5, endHour = 20): boolean {
     const now = new Date();
     const currentHour = now.getHours();
     return currentHour >= startHour && currentHour < endHour;
